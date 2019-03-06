df69356401f2d7148ccf68de5546955dd5044f36
nunukim <suzuki@scouty.co.jp>
2019-03-06 18:21:42 +0900

    Fixed install_requires in setup.py

--- setup.py
+++ setup.py
@@ -12,17 +12,17 @@ try:
 except IOError:
     readme = ''
 
-def _requires_from_file(filename):
-    return open(filename).read().splitlines()
-
 # version
 here = os.path.dirname(os.path.abspath(__file__))
 version = next((line.split('=')[1].strip().replace("'", '')
-                for line in open(os.path.join(here,
-                                              'daisy',
-                                              '__init__.py'))
+                for line in open(os.path.join(here, 'daisy', '__init__.py'))
                 if line.startswith('__version__ = ')),
                '0.0.dev0')
+
+install_requires=[
+        'luigi>=2.8.2'
+        ]
+
 
 setup(
     name="luigi-daisy",
@@ -35,9 +35,10 @@ setup(
     description='Utility wrapper of luigi',
     long_description=readme,
     packages=find_packages(),
-    install_requires=_requires_from_file('requirements.txt'),
+    install_requires=install_requires,
     license="BSD",
     classifiers=[
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
